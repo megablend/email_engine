820dfb557b4ae42173a922fda5018123f12d5412
Megafu <megafu.charles@yahoo.com>
2018-09-19 18:42:06 +0100

    Added the test case for ItemProcessing

---
+++ src/test/java/com/friendsurance/processing/ItemProcessingTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.friendsurance.processing;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Nexus Axis
+ */
+public class ItemProcessingTest {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+}
