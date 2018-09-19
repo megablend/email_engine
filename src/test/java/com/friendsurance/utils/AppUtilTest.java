a59d83fb4bd31e45cb5599ccafa231860a00c371
Megafu <megafu.charles@yahoo.com>
2018-09-19 16:56:29 +0100

    Added test cases for failed and successful properties file loading

--- src/test/java/com/friendsurance/utils/AppUtilTest.java
+++ src/test/java/com/friendsurance/utils/AppUtilTest.java
@@ -5,13 +5,70 @@
  */
 package com.friendsurance.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  *
  * @author Nexus Axis
  */
 public class AppUtilTest {
+    private String invalidPropertyFileName;
+    private String validPropertyFileName;
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Before
+    public void setup() {
+        invalidPropertyFileName = "/application.properties";
+        validPropertyFileName = "appliications.properties";
+    }
+    
+    /**
+     * Testing for invalid property files
+     * @throws Exception 
+     */
+    @Test(expected = IOException.class)
+    public void whenLoadPropertiesWithInvalidFile_thenReturnIOException() throws Exception {
+        Properties prop = new Properties();
+        InputStream is = null;
+        is = new FileInputStream(invalidPropertyFileName);
+        // load properties 
+        prop.load(is);
+    }
+    
+    /**
+     * Testing for valid property file
+     * 
+     */
     @Test
-    public void whenLoadPropertiesWithInvalidFile_thenReturnIOException() {
-        
+    public void whenLoadPropertiesWithValidFile_thenReturnOk() throws Exception {
+        Properties prop = new Properties();
+        InputStream is = null;
+        try {     
+            is = new FileInputStream("application.properties");
+            // load properties 
+            prop.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        assertThat(prop.getProperty("users_file_name"), equalTo("users.csv"));
     }
 }
