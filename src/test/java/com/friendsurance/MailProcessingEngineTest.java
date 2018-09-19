88109bf3fa37c898af00e330292ec138a5c8c410
Megafu <megafu.charles@yahoo.com>
2018-09-19 19:24:09 +0100

    Added a test case for the scheduler

--- src/test/java/com/friendsurance/MailProcessingEngineTest.java
+++ src/test/java/com/friendsurance/MailProcessingEngineTest.java
@@ -1,5 +1,10 @@
 package com.friendsurance;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +17,19 @@ package com.friendsurance;
  */
 public class MailProcessingEngineTest {
     
+    @Test
+    public void whenRunTask_thenReturnValue() throws Exception {
+        final String[] processRunning = {""};
+        TimerTask task = new TimerTask() {
+            public void run() {
+                processRunning[0] = "The task is working";
+            }
+        };
+        Timer timer = new Timer();
+
+        timer.schedule(task, 1000L);
+        Thread.sleep(1000L * 2); 
+        timer.cancel();
+        assertEquals("The task is working", processRunning[0]);
+    }
 }
