edb460510378912cf9948dfe518b1630b98e074a
Megafu <megafu.charles@yahoo.com>
2018-09-19 18:49:08 +0100

    Added test cases for valid and invalid email address

--- src/test/java/com/friendsurance/processing/ItemProcessingTest.java
+++ src/test/java/com/friendsurance/processing/ItemProcessingTest.java
@@ -5,7 +5,11 @@
  */
 package com.friendsurance.processing;
 
+import com.friendsurance.mail.EmailService;
 import java.util.regex.Pattern;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -13,4 +17,39 @@ import java.util.regex.Pattern;
  */
 public class ItemProcessingTest {
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private String validEmail;
+    private String invalidEmail;
+    
+    @Before
+    public void setup() {
+        validEmail = "mega@gmail.com";
+        invalidEmail = "xxxxx";
+    }
+    /**
+     * Test case for invalid email address
+     * @throws Exception 
+     */
+    @Test
+    public void whenInvalidEmail_thenReturnFalse() throws Exception {
+        assertTrue(!EMAIL_PATTERN.matcher(invalidEmail).matches());
+    }
+    
+    /**
+     * Test case for invalid email address
+     * @throws Exception 
+     */
+    @Test
+    public void whenValidEmail_thenReturnFalse() throws Exception {
+        assertTrue(EMAIL_PATTERN.matcher(validEmail).matches());
+    }
+    
+    /**
+     * Get the mail type priority
+     * @param mailType
+     * @return 
+     */
+    private int getPrioityNumber(EmailService.MailType mailType) {
+        String[] splitPriorityNumber = mailType.toString().split("_");
+        return Integer.valueOf(splitPriorityNumber[2]);
+    }
 }
